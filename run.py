bb78577ee86f8e66b09a25a3ffe4229ce5352f3f
Markus Snellman <ma6662sn@fys.local>
2024-04-15 20:23:49 +0200

    changed run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from app import run_app
 
-if __name__=="__main__":
-    app = run_app()
-    server = app.server
-    app.run_server(debug=False)
+app = run_app()
+server = app.server
+# app.run_server(debug=False)
